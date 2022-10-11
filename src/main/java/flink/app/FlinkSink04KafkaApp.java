71ef3b70376d7fb20c581afc08ca3c278bc17169
胡杰 <2252796369@qq.com>
2022-10-11 09:05:10 +0800

    flink联合kafka（部分完成）

--- src/main/java/flink/app/FlinkSink04KafkaApp.java
+++ src/main/java/flink/app/FlinkSink04KafkaApp.java
@@ -1,9 +1,12 @@
 package flink.app;
 
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 
 import java.util.Properties;
@@ -15,8 +18,8 @@ import java.util.Properties;
  * @author: huJie
  * @create: 2022-10-10 17:52
  **/
-public class FlinkSink03RedisApp {
-    public static void main(String[] args) {
+public class FlinkSink04KafkaApp {
+    public static void main(String[] args) throws Exception {
 
         StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
 
@@ -32,11 +35,28 @@ public class FlinkSink03RedisApp {
         //有后台线程每隔10s检测一下Kafka的分区变化情况
         properties.setProperty("flink.partition-discovery.interval-millis","10000");
 
+        //offset重置规则
+        properties.setProperty("auto.offset.reset", "latest");
+
         FlinkKafkaConsumer<String> consumer = new FlinkKafkaConsumer<String>("xdclass-topic", new SimpleStringSchema(), properties);
         //设置从记录的消费者组内的offset开始消费
         consumer.setStartFromGroupOffsets();
 
         DataStreamSource<String> dataStreamSource = environment.addSource(consumer);
         dataStreamSource.print("kafka");
+
+        // 进行数据转换
+        DataStream<String> mapDS = dataStreamSource.map(new MapFunction<String, String>() {
+            @Override
+            public String map(String value) throws Exception {
+                return "推送kafka" + value;
+            }
+        });
+
+        //输出
+        FlinkKafkaProducer<String> flinkKafkaProducer = new FlinkKafkaProducer<>("xdclass-order", new SimpleStringSchema(), properties);
+        mapDS.addSink(flinkKafkaProducer);
+
+        environment.execute("kafka执行");
     }
 }
